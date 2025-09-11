cc3275adaa87c145c0633c72442127513a70ed2d
Lvhacker1 <lavasha2@hotmail.com>
2025-09-11 16:00:16 +0200

    add types

---
+++ src/components/RoutineForm/RoutineForm.tsx
@@ -0,0 +1,25 @@
+
+
+const RoutineForm = () => {
+    return (
+        <form>
+            <div>
+                <h2></h2>
+                <input />
+            </div>
+            <div>
+                <h3></h3>
+                <div>
+                    <input />
+                    <button></button>
+                </div>
+            </div>
+            <div>
+                <button></button>
+            </div>
+        </form>
+
+    )
+}
+
+export default RoutineForm
---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export interface RoutineItemTypes {
+    id:string;
+    name: string;
+    completed: string;
+}
+
+export interface DailyRoutineTypes {
+    id:string;
+    name: string;
+    items:  RoutineItemTypes[];
+}
