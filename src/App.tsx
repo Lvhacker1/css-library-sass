86996129c490591d5c9949ee4f481a1a247c26c7
Lvhacker1 <lavasha2@hotmail.com>
2025-09-12 15:41:31 +0200

    complete routineCard and add to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import {type DailyRoutineTypes, type NewRoutineTypes } from './types/types'
+import RoutineCard from './components/RoutineCard/RoutineCard';
+import RoutineForm from './components/RoutineForm/RoutineForm';
 
 function App() {
 
+  const [routines, setRoutines] = useState<DailyRoutineTypes[]> ([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem('routines');
+    if (saved) {
+      setRoutines(JSON.parse(saved));
+    }
+  }, []);
+
+  useEffect (() => {
+    localStorage.setItem('routines', JSON.stringify(routines))
+  }, [routines])
+
+  const onAddRoutine = (routineData: NewRoutineTypes) => {
+    const newRoutine: DailyRoutineTypes = {
+      ...routineData,
+      id: Date.now().toString()
+    };
+    setRoutines([...routines, newRoutine]);
+  };
+
+  const onDeleteRoutine = (id: string) => {
+    setRoutines(routines.filter(r => r.id !== id))
+  };
+
+  const onToggleItem= (routineId: string, itemId: string) => {
+    setRoutines(routines.map(routine => 
+      routine.id === routineId
+      ? {
+        ...routine,
+        items: routine.items.map(item =>
+          item.id === itemId
+          ? {...item, completed: !item.completed} : item
+        )
+      }
+      : routine
+    ))
+  }
+
+
+
+
   return (
     <>
-      
+    <div>
+      <RoutineForm addRoutine={onAddRoutine} />
+      {routines.map (routine => (
+        <RoutineCard
+        key={routine.id}
+        routine={routine}
+        deleteRoutine={onDeleteRoutine}
+        toggleItem={onToggleItem}
+        />
+      ))}
+    </div>
     </>
   )
 }
--- src/components/RoutineCard/RoutineCard.tsx
+++ src/components/RoutineCard/RoutineCard.tsx
@@ -1,8 +1,27 @@
 import type { DailyRoutineTypes } from "../../types/types";
 
-const RoutineCard = ()=> {
+interface Props {
+    routine: DailyRoutineTypes;
+    deleteRoutine: (id:string) => void;
+    toggleItem: (routineId: string, itemId: string) => void;
+}
+
+const RoutineCard = ({routine, deleteRoutine, toggleItem}: Props) => {
     return (
         <div>
+            <div>
+                <h3>{routine.name}</h3>
+                <button onClick={() => deleteRoutine(routine.id)}></button>
+            </div>
+            {routine.items.map(item => (
+                <div key={item.id}>
+                    <input
+                    type="checkbox"
+                    checked={item.completed}
+                    onChange={() => toggleItem(routine.id, item.id)} />
+                    <span>{item.name}</span>
+                </div>
+            ))}
 
         </div>
     )
